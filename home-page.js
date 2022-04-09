47577985a130c3b17d0c0fafaea6282879d6b5b2
smeu-dragos2000 <56107332+smeu-dragos2000@users.noreply.github.com>
2022-04-09 12:53:53 +0300

    Animations test

--- home-page.js
+++ home-page.js
@@ -1,14 +1,28 @@
 let i = 0;
-let text = "MetaPhone is accelerating the world's transition to software driven cities with cloud phones, data management and performance SaaS integrated platform."
+let text = "MetaPhone is accelerating the world's transition to software driven cities with cloud phones, data management and performance SaaS integrated platform.";
+let textLength = text.length;
 const homePage1 = document.getElementById("type-effect-1");
+const typingEffectSquare = document.getElementById("typing-effect-square");
 
+
+console.log(typingEffectSquare.style.display)
 const typedEffect = () => {
     if (i < text.length) {
         homePage1.innerHTML += text.charAt(i);
         i++;
-        setTimeout(typedEffect, 10);
-        console.log(homePage1.innerHTML);
+        typingEffectSquare.style.display = "inline";
+        setTimeout(typedEffect, 5);
+    }
+    else {
+        typingEffectSquare.style.display = "none";
     }
 }
-
 window.onload = typedEffect();
+
+
+// ___________________
+
+let section3 = $('.section-3').offset().top;
+
+// ___________________
+
